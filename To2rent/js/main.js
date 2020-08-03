8b12266980d938d738475dbfa54c455433a33110
Volodymyr <kovalivvova22@gmail.com>
2020-08-03 23:03:28 +0300

    Commit To2rent
    Landing "to2rent"

---
+++ To2rent/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev"></button>',
+        responsive: [
+            {
+                breakpoint: 441,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+
+
+    });
+});
